1a0a87b7d9c5bc9abaa9f92f28138007033aa3ce
adityapatel-mri <aditya.patel@mrisoftware.com>
2025-02-24 09:54:39 +0530

    Name validation added.

--- Controllers/apis/Authentication/AuthController.cs
+++ Controllers/apis/Authentication/AuthController.cs
@@ -25,7 +25,7 @@ namespace LMS_Backend.Controllers.apis.Authentication
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto model)
         {
-            if (!IsValidEmail(model.Email) || !IsValidPassword(model.Password))
+            if (!IsValidEmail(model.Email) || !IsValidPassword(model.Password) || IsVaildName(model.Name))
             {
                 return BadRequest(new { message = "Invalid email or password." });
             }
